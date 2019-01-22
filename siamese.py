dd3a6fb521be0c048901f398523c9c19e16e33fc
ABuzin <abuzin@croc.ru>
2019-01-22 15:34:37 +0300

    Refactor the core

--- siamese.py
+++ siamese.py
@@ -3,7 +3,7 @@ from __future__ import division
 from __future__ import print_function
 
 import numpy as np
-import os
+import pandas as pd
 
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
@@ -33,63 +33,53 @@ class Siamese(object):
         self.model.summary()
         self.model.compile(optimizer=Adam(learning_rate), loss=triplet_loss(margin, strategy))
 
-        whales_data = np.genfromtxt(csv, dtype=str, delimiter=',', skip_header=True)
-        # TODO check if labels are replaced with numbers
-        # TODO alternatively can just work with raw whales dataset
+        whales_data = Siamese._read_csv(csv)
         whales = WhalesSequence(img_dir, input_shape=self.input_shape, x_set=whales_data[:, 0], y_set=whales_data[:, 1], batch_size=batch_size)
         self.model.fit_generator(whales, epochs=epochs, callbacks=[ModelCheckpoint(filepath='checkpoint-{epoch:02d}')])
         self.model.save('autosave_model.h5')
 
-    def make_embeddings(self, img_dir, batch_size=10):
-        whales_data = np.array(os.listdir(img_dir))
-        whales = WhalesSequence(img_dir, input_shape=self.input_shape, x_set=whales_data, batch_size=batch_size)
-        self.embeddings = self.model.predict_generator(whales, verbose=1)
-        # TODO make postprocessing
+    def make_embeddings(self, csv, img_dir, batch_size=10):
+        whales_data = Siamese._read_csv(csv)
+        whales = WhalesSequence(img_dir, input_shape=self.input_shape, x_set=whales_data[:, 0], batch_size=batch_size)
+        pred = self.model.predict_generator(whales, verbose=1)
+
+        pred_df = pd.DataFrame(data=pred)
+        pred_df = pd.concat([pred_df, pd.DataFrame(data=whales_data)], axis=1)
+        pred_df = pred_df.drop(['Image'], axis=1)
+        self.embeddings = pred_df.groupby(['Id']).mean().reset_index()
+
+    def predict(self):
+        pass
 
     def save_embeddings(self, filename):
         np.save(filename, self.embeddings)
 
     def load_embeddings(self, filename):
+        self.embeddings = np.load(filename)
+
+    @staticmethod
+    def _read_csv(csv):
+        csv_data = pd.read_csv(csv)
+        whales = np.sort(csv_data['Id'].unique())
+        mapping = {}
+        for i, w in enumerate(whales):
+            mapping[w] = i
+        data = csv_data.replace({'Id': mapping})
+        return data.values()
+
+    def save_model(self):
         pass
 
-    def predict(self):
+    def load_model(self):
         pass
 
-    def save(self):
+    def save_weights(self):
         pass
 
-    def load(self):
+    def load_weights(self):
         pass
 
 
-def fit(model, img_dir, csv):
-    data = np.genfromtxt(csv, dtype=str, delimiter=',', skip_header=True)
-    train = WhalesSequence(img_dir, input_shape=(384, 512, 3), x_set=data[:, 0], y_set=data[:, 1], batch_size=10)
-    model.fit_generator(train, epochs=10, callbacks=[ModelCheckpoint(filepath='checkpoint-{epoch:02d}')])
-    model.save('model_777.h5')
-
-
-def make_embeddings(model, img_dir):
-    data = np.array(os.listdir(img_dir))
-    train = WhalesSequence(img_dir, input_shape=(384, 512, 3), x_set=data, batch_size=10)
-    emb = model.predict_generator(train, verbose=1)
-    print(emb)
-    np.save('np_embeddings.pkl', emb)
-
-
-def predict(model, img_dir):
-    data = np.array(os.listdir(img_dir))
-    test = WhalesSequence(img_dir, input_shape=(384, 512, 3), x_set=data, batch_size=10)
-    emb = model.predict_generator(test, verbose=1)
-
-
-if __name__ == '__main__':
-    m = resnet_like_33(input_shape=(384, 512, 3), embedding_size=128)
-    m.compile(optimizer=Adam(0.0001), loss=triplet_loss(margin=0.5, strategy='batch_all'))
-    fit(m, 'D:/IdeaProjects/whales/data/train', 'D:/IdeaProjects/whales/data/train_fixed.csv')
-
-    # m_trained = load_model('model.h5', custom_objects={'batch_hard': triplet_loss_batch_hard(margin=0.2)})
-    # make_embeddings(m_trained, 'D:/IdeaProjects/whales/data/train')
 
 
 

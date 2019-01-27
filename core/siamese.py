1470b7cb7d1d30d02dc1ad0044579e3b6635cd35
Andrey Buzin <anbuzin@pm.me>
2019-01-28 02:03:33 +0300

    Implement the predictions part of the pipeline

--- core/siamese.py
+++ core/siamese.py
@@ -5,6 +5,8 @@ from __future__ import print_function
 import numpy as np
 import pandas as pd
 
+import os
+
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
 
@@ -15,7 +17,8 @@ from utils.sequence import WhalesSequence
 
 
 class Siamese(object):
-    def __init__(self, model, input_shape=(384, 512, 3), embedding_size=128):
+    def __init__(self, model, strategy='batch_all', input_shape=(384, 512, 3), embedding_size=128):
+        self.strategy = strategy
         self.input_shape = input_shape
         self.embedding_size = embedding_size
         self.embeddings = None
@@ -32,14 +35,21 @@ class Siamese(object):
         else:
             raise ValueError('no such model: %s' % model_name)
 
-    def train(self, csv, img_dir, epochs=10, batch_size=10, learning_rate=0.001, margin=0.5, strategy='batch_all'):
+    def save_weights(self, filename):
+        self.model.save_weights(filename)
+
+    def load_weights(self, weights):
+        self.model.load_weights(weights, by_name=True)
+
+    def train(self, csv, img_dir, epochs=10, batch_size=10, learning_rate=0.001, margin=0.5):
         self.model.summary()
-        self.model.compile(optimizer=Adam(learning_rate), loss=triplet_loss(margin, strategy))
+        self.model.compile(optimizer=Adam(learning_rate), loss=triplet_loss(margin, self.strategy))
 
         whales_data = Siamese._read_csv(csv)
         whales = WhalesSequence(img_dir, input_shape=self.input_shape, x_set=whales_data[:, 0], y_set=whales_data[:, 1], batch_size=batch_size)
-        self.model.fit_generator(whales, epochs=epochs, callbacks=[ModelCheckpoint(filepath='checkpoint-{epoch:02d}')])
+        self.model.fit_generator(whales, epochs=epochs, callbacks=[ModelCheckpoint(filepath='checkpoint-{epoch:02d}', save_weights_only=True)])
         self.model.save('autosave_model.h5')
+        self.save_weights('autosave_weights.h5')
 
     def make_embeddings(self, csv, img_dir, batch_size=10):
         whales_data = Siamese._read_csv(csv)
@@ -50,9 +60,24 @@ class Siamese(object):
         pred_df = pd.concat([pred_df, pd.DataFrame(data=whales_data)], axis=1)
         pred_df = pred_df.drop(['Image'], axis=1)
         self.embeddings = pred_df.groupby(['Id']).mean().reset_index()
+        np.save('embeddings', self.embeddings)
 
-    def predict(self):
-        pass
+    def predict(self, img_dir):
+        assert self.embeddings is not None
+        whales_data = np.array(os.listdir(img_dir))
+        whales_seq = WhalesSequence(img_dir, input_shape=self.input_shape, x_set=whales_data, batch_size=1)
+        whales = self.model.predict_generator(whales_seq, verbose=1)
+
+        concat = np.concatenate((self.embeddings, whales), axis=0)
+        prod = np.dot(concat, np.transpose(concat))
+        sq_norms = np.reshape(np.diag(prod), (-1, 1))
+
+        dist = sq_norms - 2.0 * prod + np.transpose(sq_norms)
+        dist = np.maximum(dist, 0.0)
+        dist = dist[self.embeddings.shape[0]:, :self.embeddings.shape[0]]
+
+        predictions = np.apply_along_axis(np.argpartition, 0, dist, 5)
+        np.save('predictions', predictions)
 
     def save_embeddings(self, filename):
         np.save(filename, self.embeddings)
@@ -76,11 +101,7 @@ class Siamese(object):
     def load_model(self):
         pass
 
-    def save_weights(self):
-        pass
 
-    def load_weights(self):
-        pass
 
 
 

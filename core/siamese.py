dd5900d1370a5ab885807b7433a539410a16fd4d
Andrey Buzin <anbuzin@pm.me>
2019-02-04 17:42:25 +0300

    Refactor core

--- core/siamese.py
+++ core/siamese.py
@@ -4,24 +4,42 @@ from __future__ import print_function
 
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
 import os
+from time import strftime, gmtime
 
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint
+from sklearn.manifold import TSNE
 
 from model.resnet import resnet_like_33, resnet_like_36
 from model.shallow import mnist_5
 from loss.triplet_loss import triplet_loss
 from utils.sequence import WhalesSequence
 
+# import tensorflow as tf
+# from keras.backend.tensorflow_backend import set_session
+#
+# config = tf.ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = 0.5
+# set_session(tf.Session(config=config))
 
 class Siamese(object):
     def __init__(self, model, strategy='batch_all', input_shape=(384, 512, 3), embedding_size=128):
         self.strategy = strategy
         self.input_shape = input_shape
         self.embedding_size = embedding_size
+
         self.embeddings = None
+        self.predictions = None
+
+        self.cache_dir = os.path.join('cache', strftime("cache-%y%m%d-%H%M%S", gmtime()))
+        if not os.path.isdir(self.cache_dir):
+            os.mkdir(self.cache_dir)
+        if not os.path.isdir(os.path.join(self.cache_dir, 'training')):
+            os.mkdir(os.path.join(self.cache_dir, 'training'))
+        if not os.path.isdir(os.path.join(self.cache_dir, 'debug')):
+            os.mkdir(os.path.join(self.cache_dir, 'debug'))
         self.model = Siamese.build_model(model, input_shape, embedding_size)
 
     @staticmethod
@@ -35,24 +53,20 @@ class Siamese(object):
         else:
             raise ValueError('no such model: %s' % model_name)
 
-    def save_weights(self, filename):
-        self.model.save_weights(filename)
-
-    def load_weights(self, weights):
-        self.model.load_weights(weights, by_name=True)
-
     def train(self, csv, img_dir, epochs=10, batch_size=10, learning_rate=0.001, margin=0.5):
         self.model.summary()
         self.model.compile(optimizer=Adam(learning_rate), loss=triplet_loss(margin, self.strategy))
 
-        whales_data = Siamese._read_csv(csv)
+        whales_data = self._read_csv(csv)
         whales = WhalesSequence(img_dir, input_shape=self.input_shape, x_set=whales_data[:, 0], y_set=whales_data[:, 1], batch_size=batch_size)
-        self.model.fit_generator(whales, epochs=epochs, callbacks=[ModelCheckpoint(filepath='checkpoint-{epoch:02d}', save_weights_only=True)])
-        self.model.save('autosave_model.h5')
-        self.save_weights('autosave_weights.h5')
+        self.model.fit_generator(whales,
+                                 epochs=epochs,
+                                 callbacks=[ModelCheckpoint(filepath=os.path.join(self.cache_dir, 'training', 'checkpoint-{epoch:02d}.h5'), save_weights_only=True)])
+        self.model.save(os.path.join(self.cache_dir, 'final_model.h5'))
+        self.save_weights(os.path.join(self.cache_dir, 'final_weights.h5'))
 
     def make_embeddings(self, csv, img_dir, batch_size=10):
-        whales_data = Siamese._read_csv(csv)
+        whales_data = self._read_csv(csv)
         whales = WhalesSequence(img_dir, input_shape=self.input_shape, x_set=whales_data[:, 0], batch_size=batch_size)
         pred = self.model.predict_generator(whales, verbose=1)
 
@@ -61,8 +75,7 @@ class Siamese(object):
         pred_df = pd.concat([pred_df, whales_df], axis=1)
         pred_df = pred_df.drop(['Image'], axis=1)
         self.embeddings = pred_df.groupby(['Id']).mean().reset_index()
-        # np.save('embeddings', self.embeddings)
-        self.embeddings.to_pickle('embeddings.pkl')
+        self.save_embeddings(os.path.join(self.cache_dir, 'embeddings.pkl'))
 
     def predict(self, img_dir):
         assert self.embeddings is not None
@@ -70,7 +83,7 @@ class Siamese(object):
         whales_seq = WhalesSequence(img_dir, input_shape=self.input_shape, x_set=whales_data, batch_size=1)
         whales = self.model.predict_generator(whales_seq, verbose=1)
 
-        np.save('whales', whales)
+        np.save(os.path.join(self.cache_dir, 'debug', 'raw_predictions'), whales)
 
         embeddings = self.embeddings.drop(['Id'], axis=1)
         concat = np.concatenate((embeddings, whales), axis=0)
@@ -82,34 +95,62 @@ class Siamese(object):
         dist = dist[self.embeddings.shape[0]:, :self.embeddings.shape[0]]
 
         predictions = np.apply_along_axis(np.argpartition, 1, dist, 5)
-        np.save('predictions', predictions)
-        # TODO change all saves ro pickles for consistency
+        self.predictions = pd.DataFrame(data=predictions)
+        self.predictions = pd.concat([whales_data, self.predictions], axis=1)
+        self.predictions.columns = ['Image'] + list(range(5))
+        self.save_predictions(os.path.join(self.cache_dir, 'predictions'))
+
+    def save_weights(self, filename):
+        self.model.save_weights(filename)
+
+    def load_weights(self, filename):
+        self.model.load_weights(filename, by_name=True)
 
     def save_embeddings(self, filename):
-        np.save(filename, self.embeddings)
+        self.embeddings.to_pickle(filename)
 
     def load_embeddings(self, filename):
-        # TODO clean up this mess
-        self.embeddings = pd.DataFrame(data=np.load(filename), columns=(['Id'] + (list(range(128)))))
+        self.embeddings = pd.read_pickle(filename)
 
-    @staticmethod
-    def _read_csv(csv):
+    def save_predictions(self, filename):
+        self.predictions.to_pickle(filename)
+
+    def load_predictions(self, filename):
+        self.predictions = pd.read_pickle(filename)
+
+    def save_state(self, filename):
+        pass
+
+    def load_state(self, filename):
+        pass
+
+    def _read_csv(self, csv):
         csv_data = pd.read_csv(csv)
         whales = np.sort(csv_data['Id'].unique())
         mapping = {}
+        reverse_mapping = {}
         for i, w in enumerate(whales):
             mapping[w] = i
+            reverse_mapping[i] = [w]
         data = csv_data.replace({'Id': mapping})
+        np.save(os.path.join(self.cache_dir, 'whales_to_idx_mapping'), mapping)
+        np.save(os.path.join(self.cache_dir, 'idx_to_whales_mapping'), reverse_mapping)
         return data.values
-        # TODO cache mappings for inference
-        # TODO or basically just cache the whole state i guess
-        # TODO reverse mapping
 
-    def save_model(self):
-        pass
+    @staticmethod
+    def draw_tsne(vectors):
+        tsne = TSNE(n_components=3, verbose=1, n_iter=300).fit_transform(vectors)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(tsne[:, 0], tsne[:, 1], tsne[:, 2])
+        plt.show()
 
-    def load_model(self):
-        pass
+    def make_kaggle_csv(self):
+        mapping = np.load(os.path.join(self.cache_dir, 'idx_to_whales_mapping')).item()
+        predictions = self.predictions.replace({0: mapping, 1: mapping, 2: mapping, 3: mapping, 4: mapping})
+        print(predictions)
+
+
 
 
 
--- train.py
+++ train.py
@@ -1,7 +1,6 @@
 from core.siamese import Siamese
 
-
 model = Siamese('resnet_like_33', input_shape=(384, 512, 3), embedding_size=128, strategy='batch_all')
-model.train('data/train.csv', 'data/train', epochs=20, batch_size=6, learning_rate=0.0001, margin=1.0)
+model.train('data/train.csv', 'data/train', epochs=20, batch_size=10, learning_rate=0.0001, margin=1.0)
 
 
